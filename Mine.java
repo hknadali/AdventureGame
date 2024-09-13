685e51f02cf09f868f20562172cf4dca7572ed31
Hakan Adali <hakan17adali@gmail.com>
2024-09-13 12:30:19 +0300

    New map 'Mine' and its monster 'Snake' initialized.

---
+++ Mine.java
@@ -0,0 +1,7 @@
+
+public class Mine extends BattleLoc{
+	
+	public Mine(Player player) {
+		super(player, "Mine", new Snake(), "", 5);
+	}
+}
---
+++ Snake.java
@@ -0,0 +1,7 @@
+
+public class Snake extends Monster{
+	
+	public Snake() {
+		super(4, "Snake", (int)(Math.random()*(4)) + 3, 12, 0);
+	}
+}
